391f7359a1bae47a2b94dd1a73194e1d6af7b2c3
Abdoulaye Djibril Diallo <koubia8@gmail.com>
2021-11-03 08:47:04 +0000

    iframe

---
+++ src/components/pick365/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <iframe
+    id="pick"
+    class="peak"
+    src="https://pick-365.com/broadcast/"
+    sandbox="allow-same-origin allow-forms allow-scripts"
+    scrolling
+    allowfullscreen
+    width="100%"
+    height="2500px"
+  ></iframe>
+</template>
+
+<script>
+export default {
+  mounted() {
+    var doc = document.getElementById("pick");
+    doc.style.display = "none";
+  },
+};
+</script>
+
+<style>
+.peak > html {
+  display: none !important;
+  width: 50%;
+}
+</style>
---
+++ src/pages/Test.vue
@@ -0,0 +1,14 @@
+<template>
+  <pick-365 />
+</template>
+
+<script>
+import Pick365 from "@/components/pick365";
+export default {
+  components: {
+    Pick365,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
